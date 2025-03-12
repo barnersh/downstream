4f0d4371a4d8f1b5a09e26af18c46eac98b13f2c
ElvisZ <ElvisZ>
2025-03-12 11:32:14 +0800

    feat: add tg notify

---
+++ .github/scripts/send-telegram-notify.js
@@ -0,0 +1,15 @@
+const url = `https://api.telegram.org/bot6294043429:AAHfKoVxMeCEwrNvG9G2zHFhwhSqWmPU5Ys/sendMessage`;
+
+fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        chat_id: '-1001804960926',
+        text: 'tg-deply-notify'
+    })
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
